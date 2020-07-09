7f0fede1725c52cbcd0656a5850c5fdf01e58156
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-07-09 21:12:16 +0200

    name change

--- R/retroharmonize.R
+++ R/retroharmonize.R
@@ -1,6 +1,6 @@
 #' retroharmonize: A package to create tidy Eurobarometer microdata files.
 #'
-#' The goal of surveyharmonize is to allow the organization of data 
+#' The goal of retroharmonize is to allow the organization of data 
 #' joins or panels from various data sources, particularly survey 
 #' microdata files, by retrospective harmonization the value codes, 
 #' the value labels, and the missing value ranges of the data in a 
