1e7c5c6e8c13b91d398dcf6f80085a5bf64c777c
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-07-20 09:02:15 +0200

    much updated metadata create

--- R/metadata_create.R
+++ R/metadata_create.R
@@ -1,5 +1,24 @@
 #' Create a metadata table
 #' 
+#' Create a metadata file from your surveys.
+#' 
+#' @details The structure of the returned tibble:
+#' \describe{
+#'   \item{filename}{The original file name.}
+#'   \item{id}{The ID of the survey, if present.}
+#'   \item{var_name_orig}{The original variable name in SPSS.}
+#'   \item{class_orig}{The original variable class after importing with\code{\link[haven]{read_spss}}.}
+#'   \item{label_orig}{The original variable label in SPSS.}
+#'   \item{labels}{A list of the value labels.}
+#'   \item{valid_labels}{A list of the value labels that are not marked as missing values.}
+#'   \item{na_labels}{A list of the value labels that refer to user-defined missing values.}
+#'   \item{na_range}{An optional range of a continous missing range, if present in the vector.}
+#'   \item{n_labels}{Number of categories or unique levels, which may be different from the sum of missing and category labels.}
+#'   \item{n_valid_labels}{Number of categories in the non-missing range.}
+#'   \item{n_na_labels}{Number of categories of the variable, should be the sum of the former two.}
+#'   \item{na_levels}{A list of the user-defined missing values.}
+#' }
+#' 
 #' @param survey A survey data frame.
 #' @importFrom tibble tibble
 #' @importFrom dplyr left_join mutate case_when group_by
@@ -20,9 +39,16 @@
 metadata_create <- function( survey ) {
   var_name_orig <- NULL
   
+  filename <- attr(survey, "filename")
+  if (is.null(filename)) filename <- "unknown"
+  id <- attr(survey, "id")
+  if (is.null(id)) id <- "missing"
+  
   label_orig <- as.character(sapply ( survey, labelled::var_label))
   
   metadata <- tibble::tibble (
+    filename = filename, 
+    id = id,
     var_name_orig = names(survey), 
     class_orig =  as.character(sapply( survey, function(x) class(x)[1])), 
     label_orig = ifelse ( vapply(label_orig, is.null, logical(1)), 
@@ -36,41 +62,48 @@ metadata_create <- function( survey ) {
     labelled::val_labels(x)[!labelled::val_labels(x) %in% labelled::na_values(x)]
   }
   
+  na_labels <- function(x) {
+    # labels that refer to na_values
+    labs <- labelled::val_labels(x)
+  labs[labs == labelled::na_values(x)]
+  }
+  
   to_list_column <- function(.f = "na_values") {
     
     x <- dplyr::case_when ( 
-      .f == "na_values" ~ sapply ( survey, labelled::na_values), 
+      .f == "na_labels" ~ sapply ( survey, na_labels), 
       .f == "na_range"  ~ sapply ( survey, labelled::na_range), 
       .f == "valid_range"  ~ sapply ( survey, fn_valid_range),
       .f == "labels" ~ sapply ( survey, labelled::val_labels))
     
-    x[sapply(x, is.null)] <- ""
-   
-    df <- purrr::map(x, list)
-    names(df) <- rep(.f, length(df))
-    df
+    x[sapply(x, is.null)] <- NA_character_
+    names(x) <- names(survey)
+    #df <- purrr::map(x, list)
+    #names(df) <- rep(.f, length(df))
+    x
   }
+  
+  aaa <- to_list_column( .f = "na_labels")
+
   
   range_df  <- tibble::tibble (
     var_name_orig = names(survey),
     labels = to_list_column(.f = "labels"),
-    valid_range = to_list_column(.f = "valid_range"),
-    na_values = to_list_column(.f = "na_values"),
-    na_range = to_list_column (.f = "na_range"))
+    valid_labels = to_list_column(.f = "valid_range"),
+    na_labels = to_list_column(.f = "na_labels"),
+    na_range = to_list_column (.f = "na_range")
+    )
   
-  range_df$n_labels <- vapply(1:nrow(range_df), function(x) length(unlist(range_df$labels[x])), numeric(1))
-  range_df$n_cat_labels <- vapply(1:nrow(range_df), function(x) length(unlist(range_df$valid_range[x])), numeric(1))
-  range_df$n_na_values <- vapply(1:nrow(range_df), function(x) length(unlist(range_df$na_values[x])), numeric(1))
+  label_length <- function(x) {
+    
+    ifelse ( is.na(x[[1]])[1] | length(x[[1]]) ==0,
+             0, length(x[[1]]) )
+  }
   
-  range_df$n_labels <- ifelse ( sapply ( range_df$labels, function(x) nchar(unlist(x))[1] ) == 0, 
-                                   0,range_df$n_labels)
-  range_df$n_cat_labels <- ifelse ( sapply ( range_df$valid_range, function(x) nchar(unlist(x))[1] ) == 0, 
-                                   0,range_df$n_cat_labels)
-  
-  range_df$n_na_values <- ifelse ( sapply ( range_df$na_values, function(x) nchar(unlist(x))[1] ) == 0, 
-           0,range_df$n_na_values)
-  
- 
+  range_df$n_labels <- vapply(1:nrow(range_df), function(x) label_length(range_df$labels[x]), numeric(1))
+  range_df$n_valid_labels <- vapply(1:nrow(range_df), function(x) label_length(range_df$valid_labels[x]), numeric(1))
+  range_df$n_na_labels <- vapply(1:nrow(range_df), function(x) label_length(range_df$na_labels[x]), numeric(1))
+
   return_df <- metadata %>%
     dplyr::left_join ( range_df %>% 
                          dplyr::group_by ( var_name_orig ) %>%
@@ -78,8 +111,8 @@ metadata_create <- function( survey ) {
                        by = "var_name_orig") %>%
     tidyr::unnest ( cols = "data" )  %>%
     dplyr::ungroup() %>%
-    dplyr::mutate ( n_na_values = as.numeric(n_na_values), 
-             n_cat_labels = as.numeric(n_cat_labels), 
+    dplyr::mutate ( n_na_labels = as.numeric(n_na_labels), 
+             n_valid_labels = as.numeric(n_valid_labels), 
              n_labels = as.numeric(n_labels)) %>%
     as.data.frame()
 }
--- tests/testthat/test-metadata_create.R
+++ tests/testthat/test-metadata_create.R
@@ -7,24 +7,31 @@ example_metadata <- metadata_create (
  survey = test_survey
 )
 
-example_metadata$n_na_values
+names(example_metadata)
 
 q_labels <- length(labelled::val_labels ( test_survey$qd6.12))
-q_na     <- length(labelled::na_values ( test_survey$qd6.12))
+q_na     <- length(labelled::na_values  ( test_survey$qd6.12))
 
 test_value <- example_metadata[which ( example_metadata$var_name_orig == "qd6.12"), ]
 q_labels-q_na
 
+test_value2 <- example_metadata[which ( example_metadata$var_name_orig == "qg8"), ]
 
 test_that("Correct values are returned", {
-  expect_equal(as.character(unlist(example_metadata$na_values[2])), "")
+  expect_equal(unique(example_metadata$filename), "ZA7576.rds")
+  expect_equal(as.character(unlist(example_metadata$na_labels[2])), NA_character_)
   expect_equal(example_metadata$label_orig[1], "unique identifier in za7576")
   expect_equal(
-    length(test_value$na_values), 
+    length(test_value$na_labels), 
     q_na)
   expect_equal(
-    test_value$n_cat_labels, 
+    test_value$n_valid_labels, 
     q_labels-q_na)
   expect_equal(example_metadata$var_name_orig[1], "rowid")
+  expect_equal(c(
+    length(test_value2$labels[[1]]), 
+    length(test_value2$valid_labels[[1]]),
+    length(test_value2$na_labels[[1]])
+  ), c(8,6,2))
 })
 
