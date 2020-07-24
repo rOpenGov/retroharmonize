be6fdb9b6f89e09d0cc88b7384d2c681929b13a5
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-07-24 11:49:36 +0200

    natural full join extra columns rewritten

--- R/harmonize_waves.R
+++ R/harmonize_waves.R
@@ -24,54 +24,63 @@ harmonize_waves <- function(waves, .f) {
   numerics <- unique(names(classes[which(classes %in% c("numeric", "double", "integer"))]))
   characters <- unique(names(classes[which(classes %in% c("character"))]))
 
+  dat <- waves[[1]]
   extend_survey <- function (dat ) {
     
     to_add_rh <- retroharmonized[which(!retroharmonized %in% names(dat))]
     to_add_numerics <- numerics[which(!numerics %in% names(dat))]
     to_add_characters <- characters[which(!characters %in% names(dat))]
     
-    add_numeric_df <- as.data.frame( matrix (length(to_add_numerics), nrow(dat))) %>%
-      setNames(to_add_numerics)
-    
     if ( length(to_add_numerics)>0) {
-      dat <- dat %>% bind_cols(as_tibble ( matrix ( rep( NA_real_, 
-                                length(to_add_numerics)*nrow(dat)), 
-                           nrow = nrow(dat))) %>%
-        stats::setNames(nm = to_add_numerics ))
+      ## There are numeric values in other surveys that need to be 
+      ## added with NA_real_ values here.
+      
+      add_numeric_df <- as.data.frame( 
+        matrix (rep( NA_real_,
+                     length(to_add_numerics)*nrow(dat)), 
+                     nrow = nrow(dat), 
+                     ncol = length(to_add_numerics)) 
+        ) %>%
+        stats::setNames(to_add_numerics)
+      
+      dat <- dat %>% 
+        bind_cols(add_numeric_df) 
     }
     
     if ( length(to_add_characters)>0) {
-      dat <- dat %>% bind_cols(as_tibble (
+      
+      add_character_df <- as.data.frame(
         matrix ( rep( NA_character_,
                       length(to_add_characters)*nrow(dat)), 
-                       nrow = nrow(dat))) %>%
-                 stats::setNames(nm = to_add_characters ))
+                 nrow = nrow(dat)) %>%
+          stats::setNames(to_add_numerics)
+      )
+     
+      dat <- dat %>% 
+        bind_cols(add_character_df) 
     }
     
     if ( length(to_add_rh)>0 ) {
       
-      
       fn_inap <- function(x) haven::labelled_spss(x,
-                                             labels = c(inap = 999999), 
-                                             na_values = 999999)
-        
-  
-      addition <-  as_tibble ( 
-        matrix ( rep( 99999, 
-                      length(to_add_rh)*nrow(dat)),
-                 nrow = nrow(dat))
-      ) %>%
-        stats::setNames(nm = to_add_rh ) %>%
-        mutate_all ( fn_inap )
+                                                  labels = c(inap = 999999), 
+                                                  na_values = 999999)
       
-      for ( i in ncol(addition)) {
-        attr( addition[,i], "label") <- to_add_rh[i]
+      add_rh_df <- as.data.frame( 
+        matrix (rep( 999999,
+                     length(to_add_rh)*nrow(dat)), 
+                     ncol = length(to_add_rh),
+                     nrow = nrow(dat))
+        ) %>%
+        stats::setNames(to_add_rh) %>%
+        mutate_all ( fn_inap )
+     
+      for ( i in ncol(add_rh_df)) {
+        attr( add_rh_df[,i], "label") <- to_add_rh[i]
       }
       
-      dat <- dat %>% bind_cols(
-        as_tibble(
-        lapply ( addition, function(x) as_labelled_spss_survey(x, attr(dat, "id")))
-        ))
+      dat <- dat %>%
+        bind_cols(add_rh_df)
     }
     
     if ( ! all(sort(names ( dat )) == sort(all_names))) {
