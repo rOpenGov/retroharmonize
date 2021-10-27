ba66329d1ba88b4ef9b0ad365a8de6ef789af6d7
dieghernan <diego.hernangomezherrero@gmail.com>
2021-10-27 09:13:14 +0000

    Add test for generic `summary`

--- tests/testthat/test-survey.R
+++ tests/testthat/test-survey.R
@@ -11,3 +11,19 @@ test_that("correct casting", {
   expect_true(is.survey(example_survey))
   expect_equal(attr(example_survey, "filename"), "no_file")
 })
+
+
+test_that("summary.survey generic produces the same results", {
+  expect_true(inherits(example_survey, "survey"))
+  expect_true(is.survey(example_survey))
+  
+  positional <- capture_output(summary(example_survey))
+  named <- capture_output(summary(df = example_survey))
+  
+  expect_identical(named, positional)
+  
+  # Use named object
+  namedobj <- capture_output(summary(object = example_survey))
+  expect_identical(named, namedobj)
+  
+})
